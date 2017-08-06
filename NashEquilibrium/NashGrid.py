a484c52034ebd94dc726f5d25fde5700bce41c4d
david138 <luke.davidson@mail.utoronto.ca>
2017-08-06 17:56:09 -0400

    Uploaded NashGrid Class
    The NashGrid class takes a file containing a Nash table (Example in README). It creates a 2D array from from the file that represents the Nash table. It has some getters to obtain specifications about the grid.

---
+++ NashEquilibrium/NashGrid.py
@@ -0,0 +1,46 @@
+P1 = 0
+P2 = 1
+
+class NashGrid:
+    def __init__(self, addr):
+        file = open(addr, "r")
+        self.payout_grid = self.generate_payout_grid(file)
+        self.row_labels = self.generate_labels(len(self.payout_grid))
+        self.col_labels = self.generate_labels(len(self.payout_grid[0]))
+        file.close()
+
+    def generate_payout_grid(self, file):
+        payout_grid = []
+        for line in file:
+            row_array = []
+            for payouts in line.split(" "):
+                row_array.append([int(payout) for payout in payouts.split(",")])
+            payout_grid.append(row_array)
+        return payout_grid;
+
+    def generate_labels(self, labels_num):
+        return [i for i in range(labels_num)];
+
+    def get_payout_grid(self):
+        return self.payout_grid;
+
+    def set_payout_grid(self, payout_grid):
+        self.payout_grid = payout_grid
+
+    def get_rows(self):
+        return len(self.payout_grid);
+
+    def get_cols(self):
+        return len(self.payout_grid[0]);
+
+    def get_row_labels(self):
+        return self.row_labels;
+
+    def set_row_labels(self, row_labels):
+        self.row_labels = row_labels
+
+    def get_col_labels(self):
+        return self.col_labels;
+
+    def set_col_labels(self, col_labels):
+        self.col_labels = col_labels
