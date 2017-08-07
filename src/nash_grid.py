55442a09547fbab5a35c9ebb835e9c0c674e1838
david138 <lukedavidson06@gmail.com>
2017-08-07 14:34:18 -0400

    fix bugs in remove_dominated_moves

--- src/nash_grid.py
+++ src/nash_grid.py
@@ -22,43 +22,37 @@ class NashGrid:
         return [i for i in range(labels_num)];
 
     def remove_dominated_moves(self):
-        removed_total = 0
-        removed_num = -1
-        while (removed != 0): 
-            removed_num = self.remove_dominated_p1()
-            removed_num += self.removeDominatedP2()
-            removed_total += removed_num
-        return ("Optimized! " + removed_total + " moves removed");
+        while self.remove_dominated_p1() || self.remove_dominated_p2()
 
     def remove_dominated_p1(self):
         rows_to_keep = set()
-        rows = len(self.payout_grid)
-        cols = len(self.payout_grid[0])
-        for c in range(cols):
-            max_payout = max([self.payout_grid[r][c][P1] for r in range(rows)])
-            for r in range(rows):
+        row_num = len(self.payout_grid)
+        col_num = len(self.payout_grid[0])
+        for c in range(col_num):
+            max_payout = max([self.payout_grid[r][c][P1] for r in range(row_num)])
+            for r in range(row_num):
                 if (self.payout_grid[r][c] == max_payout):
                     rows_to_keep.update(r)
 
         new_payout_grid = [self.payout_grid[i] for i in sorted(rows_to_keep)]
         self.payout_grid = new_payout_grid
         self.row_labels = [self.row_labels[i] for i in sorted(rows_to_keep)]
-        return rows - len(rows_to_keep);
+        return row_num != len(rows_to_keep);
 
     def remove_dominated_p2(self):
         cols_to_keep = set()
-        rows = len(self.payout_grid)
-        cols = len(self.payout_grid[0])
-        for r in range(rows):
-            max_payout = max([self.payout_grid[r][c][P2] for c in range(cols)])
-            for c in range(cols):
+        row_num = len(self.payout_grid)
+        col_num = len(self.payout_grid[0])
+        for r in range(row_num):
+            max_payout = max([self.payout_grid[r][c][P2] for c in range(col_num)])
+            for c in range(col_num):
                 if (self.payout_grid[r][c] == max_payout):
                     cols_to_keep.update(c)
 
-        new_payout_grid = [[] for i in range(rows)]
+        new_payout_grid = [[] for i in range(row_num)]
         for c in sorted(cols_to_keep):
             for r in new_payout_grid:
                 r.append(self.payout_grid[r][c])
         self.payout_grid = new_payout_grid
         self.col_labels = [self.col_labels[i] for i in sorted(cols_to_keep)]
-        return cols - len(cols_to_keep);
+        return col_num != len(cols_to_keep);
